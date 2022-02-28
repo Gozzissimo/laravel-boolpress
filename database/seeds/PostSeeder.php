e6723c9aaafcb7971f0f01f8e9e2869cd4e4e7b3
Gozzissimo <fantini.nico@gmail.com>
2022-02-28 21:22:32 +0100

    Model created

---
+++ database/seeds/PostSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Faker/Generator as Faker;
+
+use App\Admin\Model\Post;
+use Illuminate\Database\Seeder;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
