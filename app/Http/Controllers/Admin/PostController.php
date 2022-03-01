673b07c39f34d318b6ecdba1cb993a508507ba70
Gozzissimo <fantini.nico@gmail.com>
2022-03-01 15:27:23 +0100

    show

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -74,7 +74,7 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        dd($post);
+        return view('admin.posts.show', ['post' => $post]);
     }
 
     /**
---
+++ resources/views/admin/posts/show.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>
+                    {{$post->title}}
+                </h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                {{$post->content}}
+            </div>
+        </div>
+    </div>
+@endsection
