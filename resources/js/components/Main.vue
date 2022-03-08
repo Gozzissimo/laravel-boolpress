32f6a698755cfa94c4536b49ec446bed9d6b56bd
Gozzissimo <fantini.nico@gmail.com>
2022-03-08 19:14:39 +0100

    stampa in pagina cards

--- resources/js/components/Main.vue
+++ resources/js/components/Main.vue
@@ -1,13 +1,17 @@
 <template>
-    <div class="card-group">
-        <div class="card">
-            <img src="" class="card-img-top" alt="...">
-            <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
-            <div class="card-footer">
-            <small class="text-muted">Last updated 3 mins ago</small>
+    <div class="row row-cols-1 row-cols-md-2 g-4">
+        <div 
+        v-for="(post, index) in posts"
+        :key="index"
+        class="col">
+            <div class="card mb-4 h-100">
+                <div class="card-body">
+                <h5 class="card-title">{{ post.title }}</h5>
+                <p class="card-text">{{ post.content }}</p>
+                </div>
+                <div class="card-footer">
+                <small class="text-muted">Last updated {{ post.updated_at }}</small>
+                </div>
             </div>
         </div>
     </div>
@@ -18,11 +22,17 @@ import Axios from 'axios'
 
 export default {
     name: 'Main',
+    data() {
+        return {
+            posts: []
+        }
+    },
 
     created() {
         Axios.get('http://127.0.0.1:8000/api/posts').then(
             (result) => {
-                console.log(result.data.results.posts[0]);
+                // console.log(result.data.results.posts[0]);
+                this.posts = result.data.results.posts
             }
         );
     }
